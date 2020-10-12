abadd3f2973b77a37a9087ac97b18574180553fc
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-10-12 13:13:07 -0300

    improve project image hover

--- src/components/ProjectItem/styled.js
+++ src/components/ProjectItem/styled.js
@@ -8,6 +8,17 @@ export const Wrapper = styled.div`
 
 export const LinkTo = styled(Link)`
   text-decoration: none;
+  &:hover {
+    h2 {
+      color: var(--primary-color);
+      transition: 0.2s;
+    }
+
+    & > div:first-child {
+      box-shadow: 0 0 10px rgba(144,70,254,.5);
+      transition: 0.2s;
+    }
+  }
 `;
 
 export const Media = styled.div`
@@ -16,7 +27,7 @@ export const Media = styled.div`
 export const Content = styled.div`
 `;
 
-export const Title = styled.div`
+export const Title = styled.h2`
   margin: var(--space) 0 var(--space-sm);
 `;
 export const Description = styled.p`
